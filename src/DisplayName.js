d96fd22ecdd57ec1a1921a0e7a78abef9519cf80
Sajal Mandal <sajal1294@gmail.com>
2025-04-24 12:25:07 +0530

    test 6

--- src/DisplayName.js
+++ src/DisplayName.js
@@ -30,24 +30,12 @@ function DisplayNAme(){
             <h1>Full Name Display</h1>
             <label htmlFor="FirstName">First Name : </label>
             <input id="FirstName"type="text" value={firstName} placeholder="Enter First Name" onChange={(e)=>setFirstName(e.target.value)} required></input>
-            {
-                errors.firstName && (
-                    <p style={{
-                        color:"red"
-                    }}>{errors.firstName}</p>
-                )
-            }
+
             
             <br/>
             <label htmlFor="LastName">Last Name : </label>
             <input id="LastName" type="text" value={lastName} placeholder="Enter First Name" onChange={(e)=>setLastName(e.target.value)} required></input>
-            {
-                errors.lastName && (
-                    <p style={{
-                        color:"red"
-                    }}>{errors.lastName}</p>
-                )
-            }
+
             <br/>
             <button type="submit"  >Submit</button>
 
