b24d741be402b387e704b90de8e1ab8ab99a5baf
Sajal Mandal <sajal1294@gmail.com>
2025-04-24 12:11:23 +0530

    Test 4

--- src/DisplayName.js
+++ src/DisplayName.js
@@ -6,35 +6,12 @@ function DisplayNAme(){
     const [firstName,setFirstName]=useState('');
     const [lastName,setLastName]=useState('');
     const [fullName,setFullName]=useState('');
-    const [error,setError]=useState({firstName:'',lastName:''});
-    
+    const [errors,setErrors]=useState({});
+
     const validatename=(name)=>{
-        if(!name.trim())
-            return "This field is required!!";
-        if(!/^[A-Za-z]+$/.test(name.trim()))
-            return "Only letters are allowed!!";
-        return '';
-
-    }
-
-    const handleFirstName=(e)=>{
-        const value= e.target.value;
-        setFirstName(value);
-        setError((prev)=>({
-            ...prev,
-            firstName:validatename(value),
-        }));
-
+        return /^[A-Za-z]+$/.test(name);
     };
-    const handleLastName=(e)=>{
-        const value= e.target.value;
-        setLastName(value);
-        setError((prev)=>({
-            ...prev,
-            lastName:validatename(value),
-        }));
-
-    };
+    
 
 
 
@@ -42,48 +19,59 @@ function DisplayNAme(){
     const handleSubmit=(e)=>{
         e.preventDefault();
         
-        const firstNameError= validatename(firstName);
-        const lastNameError= validatename(lastName)
-
-        setError({firstName:firstNameError,lastName:lastNameError});
-        
-        if(!firstNameError && !lastNameError){
-            setFullName(`${firstName.trim()} ${lastName.trim()}`)
+        const newError={};
+        if(!firstName.trim()){
+            newError.firstName='first name is required';
         }
-        else{
-            setFullName('')
+        else if(!validatename(firstName)){
+            newError='Only letteres are allowed'
         }
 
+        if(!lastName.trim()){
+            newError.lastName='first name is required';
+        }
+        else if(!validatename(lastName)){
+            newError='Only letteres are allowed'
+        }
+        setErrors(newError);
+
+        if(Object.keys(newError).length===0){
+            setFullName(`${firstName} ${lastName}`);
+        }
+
+        else {
+            setFullName("");
+        }
     };
 
     
 
     return(
         <div>
-            <form >
+            <form onSubmit={handleSubmit}>
             <h1>Full Name Display</h1>
             <label htmlFor="FirstName">First Name : </label>
-            <input id="FirstName"type="text" value={firstName} placeholder="Enter First Name" onChange={handleFirstName} required></input>
+            <input id="FirstName"type="text" value={firstName} placeholder="Enter First Name" onChange={(e)=>setFirstName(e.target.value)} required></input>
             {
-                error.firstName && (
+                errors.firstName && (
                     <p style={{
                         color:"red"
-                    }}>{error.firstName}</p>
+                    }}>{errors.firstName}</p>
                 )
             }
             
             <br/>
             <label htmlFor="LastName">Last Name : </label>
-            <input id="LastName" type="text" value={lastName} placeholder="Enter First Name" onChange={handleLastName} required></input>
+            <input id="LastName" type="text" value={lastName} placeholder="Enter First Name" onChange={(e)=>setLastName(e.target.value)} required></input>
             {
-                error.lastName && (
+                errors.lastName && (
                     <p style={{
                         color:"red"
-                    }}>{error.lastName}</p>
+                    }}>{errors.lastName}</p>
                 )
             }
             <br/>
-            <button type="submit" onSubmit={handleSubmit} >Submit</button>
+            <button type="submit"  >Submit</button>
 
             </form>
 
